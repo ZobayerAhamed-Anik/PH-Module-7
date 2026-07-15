1c744146401dc5cac86873741147a92b81016a06
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-07-15 19:47:25 +0600

    You can't use reserved word to name a variable

---
+++ Module-7.6/naming.js
@@ -0,0 +1,14 @@
+// Every variable name shoud be meaningful. It will indicate what it contains.
+let location = 'Bandarban';
+let age = 18;
+let boyos = 19;
+
+// These are not recommanded...
+let tugudutugudutuuaoue = 97;
+let avc = 45;
+let info = 'Mon majar';
+
+// Do not use any shorthand name for variables.
+// Its okay if the name is longer.
+const let = 26;
+console.log(let);
