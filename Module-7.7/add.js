dfdb5f31d90b686422dcba2d6618d1e6d7d47eb7
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-07-18 22:46:31 +0600

    If a string and a number is added the result is auto converted to a string.

--- Module-7.7/add.js
+++ Module-7.7/add.js
@@ -1,6 +1,9 @@
-const orangePrice = 20;
-const mangoPrice = 30;
-const totalPrice = orangePrice + mangoPrice;
+let orangePrice = 20;
+orangePrice = '20';
+let mangoPrice = 30;
+let totalPrice = orangePrice + mangoPrice;
 console.log(orangePrice);
 console.log(mangoPrice);
 console.log(totalPrice); 
+// if a string and a number is added the result is auto converted to a string.---
+// e.g. '20' + 30 = 2030 
