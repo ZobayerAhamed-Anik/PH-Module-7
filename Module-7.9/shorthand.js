246dba5d1612f9442ccea38ac1b09db9edc0f3fe
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-07-19 00:55:30 +0600

    Some shorthand practice

---
+++ Module-7.9/shorthand.js
@@ -0,0 +1,15 @@
+let price = 500;
+// price = 400;
+// price = 425;
+
+// price = price + 60;
+// console.log(price);
+
+// Shortcut for this is:
+// price += 60;
+// console.log(price);
+
+price++;
+console.log(price);
+console.log(--price);
+
