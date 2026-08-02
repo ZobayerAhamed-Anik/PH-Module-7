74c3d642b7f440fce73bcf9e900e493dded798f6
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-08-02 20:29:12 +0600

    Task 8 done

--- PracticeTask/Task7.js
+++ PracticeTask/Task7.js
@@ -8,4 +8,4 @@ console.log("Addition:", number1 + number2);
 console.log("Subtraction:", number1 - number2); 
 console.log("Multiplication:", number1 * number2); 
 console.log("Division:", number1 / number2);
-console.log("Modulus:", number1 % number2); 
+console.log("Modulus:", number1 % number2);
