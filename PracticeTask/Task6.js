328fa79d3d540696f609efd7443447660b03f41e
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-08-02 20:20:54 +0600

    Task6 done

---
+++ PracticeTask/Task6.js
@@ -0,0 +1,12 @@
+let name = "Zobayer Ahamed";
+let age = 25;
+let favoriteFood = "Pizza";
+let favoriteLanguage = "JavaScript";
+let lovesCoding = true;
+  
+console.log("Hello!"); 
+console.log("My name is " + name + "."); 
+console.log("I am " + age + " years old."); 
+console.log("My favorite food is " + favoriteFood + "."); 
+console.log("My favorite programming language is " + favoriteLanguage + "."); 
+console.log("I love coding:", lovesCoding);
