02aaca840e626c98f2514d219400293a839f908d
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-08-02 20:09:07 +0600

    Task4 done

---
+++ Practice Task/Task4.js
@@ -0,0 +1,11 @@
+let a = 100; 
+let b = "Programming"; 
+let c = true; 
+let d = false; 
+let e = 3.1416; 
+  
+console.log("Datatype of a:", typeof a); 
+console.log("Datatype of b:", typeof b); 
+console.log("Datatype of c:", typeof c); 
+console.log("Datatype of d:", typeof d); 
+console.log("Datatype of e:", typeof e); 
